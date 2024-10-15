d0aaa066c9ef2d67cc78298edd34f19432027e51
NethminiSandunika <nethminid92@gmail.com>
2024-10-15 13:42:10 +0530

    Finish the nav bar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,74 @@
 import React from 'react';
 import "./sidebar.css";
+import Logo from "../../assets/logo.svg";
 
 const Sidebar = () => {
     return(
-        <div>Sidebar</div>
+        <aside className='aside'>
+            <a href='#home' className='nav__logo'>
+                <img src={Logo} alt=""/>
+            </a>
+
+            <nav className="nav">
+                <div className="nav__menu">
+                    <ul className="nav__list">
+                        <li className="nav__item">
+                            <a href="#home" className="nav__link">
+                                <li className="icon-home"></li>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#about" className="nav__link">
+                            <li className="icon-user-following"></li>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#services" className="nav__link">
+                            <li className="icon-briefcase"></li>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#resume" className="nav__link">
+                            <li className="icon-graduation"></li>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="portfolio" className="nav__link">
+                            <li className="icon-layers"></li>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#blog" className="nav__link">
+                            <li className="icon-note"></li>
+                            </a>
+                        </li>
+
+                        <li className="nav__item">
+                            <a href="#contact" className="nav__link">
+                            <li className="icon-bubble"></li>
+                            </a>
+                        </li>
+
+                        
+
+
+                    </ul>
+
+                </div>
+
+            </nav>
+
+            <div className="nav__footer">
+                <span className="copyright">&copy; 2024.</span>
+
+            </div>
+
+        </aside>
     )
 }
 
