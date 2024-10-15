494e717f9b240ec7745afe32fb0a5c0e1a795eb3
NethminiSandunika <nethminid92@gmail.com>
2024-10-16 01:35:24 +0530

    start developing the other pages

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import "./about.css";
+import Image from "../../assets/avatar-2.svg"
+import AboutBox from './AboutBox';
 
 const About = () => {
     return(
-        <div>About</div>
-    )
-}
+        <section className="about container section" id='about'>
+            <h2 className="section__title">About Me</h2>
+
+            <div className="about__container grid">
+                <img src={Image} alt="" className="about__img" />
+
+                <div className="about__data grid">
+                    <div className="about__info">
+                        <p className="about__description">I am a dedicated third-year Software Engineering undergraduate, passionate about becoming a Quality 
+						Assurance Engineer. I am committed to utilizing my attention to detail, problem-solving skills, and teamwork 
+						abilities, along with a solid understanding of Agile methodologies, to contribute effectively in software 
+						testing and quality assurance.</p>
+
+                        <a href="" className="btn">Download CV</a>
+                    </div>
+
+
+
+
+
+
+
+
+
+    
+
+
+
+                </div>
+            </div>
+
+            <AboutBox />
+
+        </section>
+    );
+};
 
 export default About
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
 import "./home.css";
+import Me from "../../assets/avatar-1.svg";
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "./ScrollDown";
+import Shapes from "./Shapes";
 
 const Home = () => {
     return(
-        <div>Home</div>
-    )
-}
+        <section className="home container" id="home">
+            <div className="intero">
+                <img src={Me} alt="" className="home__img" />
+                <h1 className="home__name">Nethmini Sandunika</h1>
+                <span className="home__education">I'm a QA Enthusiast</span>
 
-export default Home
+                <HeaderSocials />
+
+                <a href="#contact" className="btn">Hire Me</a>
+
+            <ScrollDown />
+            </div>
+
+            <Shapes />
+        </section>
+    );
+};
+
+export default Home;
