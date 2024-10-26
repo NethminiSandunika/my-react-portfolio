17d2d700e237a9ce561e646e94cbe08e58371d5d
NethminiSandunika <nethminid92@gmail.com>
2024-10-26 22:14:43 +0530

    added a dark mode option

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './components/home/Home';
@@ -8,21 +8,42 @@ import Resume from './components/resume/Resume';
 import Portfolio from './components/portfolio/Portfolio';
 import Blog from './components/blog/Blog';
 import Contact from './components/contact/Contact';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Toggle dark mode
+  const toggleDarkMode = () => {
+    setDarkMode(prevMode => !prevMode);
+  };
+
+  // Apply dark mode class to body
+  useEffect(() => {
+    if (darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }, [darkMode]);
+
   return (
     <>
-    <Sidebar />
-    <main className='main'></main>
-    <Home />
-    <About />
-    <Services />
-    <Blog />
-    <Resume />
-    <Portfolio />
-    <Contact />
+      <Sidebar />
+      <main className='main'>
+        <button className="dark-mode-toggle" onClick={toggleDarkMode}>
+          {darkMode ? <FaSun /> : <FaMoon />}
+        </button>
+        <Home />
+        <About />
+        <Services />
+        <Blog />
+        <Resume />
+        <Portfolio />
+        <Contact />
+      </main>
     </>
-  )
-}
+  );
+};
 
 export default App;
